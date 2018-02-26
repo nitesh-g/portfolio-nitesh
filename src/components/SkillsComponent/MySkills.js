23fa920b0ed7cc1a69cfd8f40f129e5bf5d80af5
Nitesh Gupta <niteshgupta@Niteshs-MacBook-Pro.local>
2018-02-26 11:30:18 -0500

    reduced the file size of the image in assests and added some components

---
+++ src/components/SkillsComponent/MySkills.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MySkills extends Component {
+    render() {
+        return(
+            <div className="Container">
+                
+            </div>
+        );
+    }
+}
--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -1,12 +1,14 @@
 import React, { Component }  from 'react'
 import Container from '../theme/grid/Container'
+import MySkills from '../components/SkillsComponent/MySkills'
 
 export default class Projects extends Component {
 	render(){
 		return(
 			<Container>
-				<h1>Nitesh Projects</h1>
+				<h1 className="project-title-wrapper" > Nitesh Projects</h1>
+				<MySkills />
 			</Container>
-			)
+			);
 }	
 }
